2179d42340321308b81ae943adbba4f4cec1f378
apeiris <apeiris@hotmail.com>
2020-09-10 12:57:03 -0400

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+#include <WiFi.h>
+#include <WiFiMulti.h>
+#include <HTTPClient.h>
+#include "cert.h"
+#include "password.h"
+void setup() {
+  // put your setup code here, to run once:
+  printf("%s\n",discordCertificate);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
